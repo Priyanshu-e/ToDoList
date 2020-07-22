9bbd4847e8ec307cf34071182aca5daa5702f8ff
Priyanshu-e <kushpriyanshu91@gmail.com>
2020-07-22 14:02:25 +0530

    Updates

---
+++ precache-manifest.f2b7fa997126eff101e7125e89c8f1bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d4debbcc7691e4794469c184f8dcf1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "8813d3425c453fd26682",
+    "url": "/ToDoList/static/css/main.97498011.chunk.css"
+  },
+  {
+    "revision": "499ab76006c4b8ba777e",
+    "url": "/ToDoList/static/js/2.522ce014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.522ce014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8813d3425c453fd26682",
+    "url": "/ToDoList/static/js/main.1fb9bfdf.chunk.js"
+  },
+  {
+    "revision": "540aed621bf203086e43",
+    "url": "/ToDoList/static/js/runtime-main.1476aa9a.js"
+  }
+]);
