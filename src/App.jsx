0b60dfa2209746e35d4cc1bf56e1522363064fe5
Priyanshu-e <kushpriyanshu91@gmail.com>
2020-07-22 13:44:38 +0530

    my first commit webapp

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useState } from 'react';
+
+import ToDoList from './ToDoList'
+
+
+const App = () =>{
+    
+    
+    const[name, setName]= useState();
+    const[Items, setItems]= useState([]);
+    
+
+    const itemEvent = (event) =>{
+    
+        setName(event.target.value);
+    };
+    const listOfItems = () => {
+         setItems( (oldItems) =>{
+              return[...oldItems, name];
+         });
+         setName("");
+    };
+    
+    const deleteItems =(id) =>{
+        console.log("deleted");
+
+        setItems((oldItems)=>{
+            return oldItems.filter((arrElem, index)=>{
+                return index !==id;
+            })
+        })
+     }
+    
+    
+    return(
+    <>
+    <div className="main_div">
+        <div className="center_div">
+            <br />
+            <h1>ToDo List</h1>
+            <br />
+            <input type="text" placeholder="Add a item" value={name} onChange={itemEvent}/>
+            <button onClick={listOfItems}>+</button>
+
+            <ol>
+
+
+           { Items.map( (itemval, index) => {
+              return <ToDoList
+               key={index} 
+               id={index}
+               text={itemval} 
+               onSelect={deleteItems}/>;
+
+            })}
+                </ol>
+            </div>
+        </div>
+    </>
+);
+};
+
+export default App;
